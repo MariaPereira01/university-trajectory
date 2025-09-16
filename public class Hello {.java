9450a0ce7f9a921ecfa8d680ee361c1b922ff304
Maria Pereira <mariaaaa229@gmail.com>
2025-09-15 23:38:19 -0300

    Add files via upload

---
+++ public class Hello {.java
@@ -0,0 +1,15 @@
+public class Hello {
+     public static void main (String[] agrs) {
+        System.out.println("Ola Mundo!");
+        int number = 20; 
+        //int - limite de tamanho inteiro numeroGrande (variavel)//
+        //saida através do terminal//
+        long numeroMaior = 9888555552;
+        float decimal = 20.02f;
+        double duplaPrecisao = 31.000d;
+        boolean exemplo = true or false;
+        char c = 'a';
+        String name = "AUAUAUAAUAU";
+     }
+
+}
